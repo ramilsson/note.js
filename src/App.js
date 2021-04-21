e2c87f50805eea8f5f4fee17e0200d2bc1c7beae
Ruslan Khuzhin <khuzh.n@gmail.com>
2021-04-21 23:59:33 +0500

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main className='section'>
+        <div className='columns'>
+          <div className='column'>
+            <Navigation/>
+          </div>
+          <div className='column is-half'>
+            <div className='box'>
+              {/* List of notes */}
+            </div>
+          </div>
+          <div className='column'>
+            {/* Some widgets */}
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className='hero is-small is-dark'>
+      <div className='hero-body'>
+        <p className='title'>Note.js</p>
+        <p className='subtitle'>Small subtitle</p>
+      </div>
+    </header>
+  );
+}
