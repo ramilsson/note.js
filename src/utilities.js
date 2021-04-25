bf3f8a80c59048304474474014fe9e71cbf6ff21
Ruslan Khuzhin <khuzh.n@gmail.com>
2021-04-25 21:53:09 +0500

    add formatDate function

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,17 @@
 export function getRandomId(length = 8) {
-    return Math.random().toString(36).substring(2, length + 2);
+  return Math.random()
+    .toString(36)
+    .substring(2, length + 2);
+}
+
+export function formatDate(str) {
+  const date = new Date(str);
+  const formatter = new Intl.DateTimeFormat('en-US', {
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  });
+  return formatter.format(date);
 }
