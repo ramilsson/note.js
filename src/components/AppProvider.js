813fd6a57f4d2221615cbb7f87d4634178fcff36
Ruslan Khuzhin <khuzh.n@gmail.com>
2021-04-25 21:48:09 +0500

    use React Context API to store the app states

---
+++ src/components/AppProvider.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import AppContext from './AppContext';
+
+export default function AppProvider({ children }) {
+  const [folders, setFolders] = useState([]);
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetchFolders();
+    fetchNotes();
+  }, []);
+
+  function fetchFolders() {
+    fetch(`${process.env.REACT_APP_API_URL}/folders`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error();
+        }
+        return response.json();
+      })
+      .then((folders) => {
+        setFolders(folders);
+      })
+      .catch(() => alert('Could not fetch folders from the server'));
+  }
+
+  function fetchNotes() {
+    fetch(`${process.env.REACT_APP_API_URL}/notes`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error();
+        }
+        return response.json();
+      })
+      .then((notes) => {
+        setNotes(notes);
+      })
+      .catch(() => alert('Could not fetch notes from the server'));
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        folders,
+        notes,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+}
