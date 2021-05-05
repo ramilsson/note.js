fc77b7f3ab27cfcf064ddeb003dde3f8b3fc7ab6
Ruslan Khuzhin <khuzh.n@gmail.com>
2021-05-05 23:59:38 +0500

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className='page-footer hero is-small is-light'>
+      <div className='hero-body is-flex is-justify-content-center'>
+        <a href='https://bulma.io' target='_blank' rel='noreferrer'>
+          <img className='is-block' src='https://bulma.io/images/made-with-bulma.png' alt='Made with Bulma' width='128' height='24' />
+        </a>
+      </div>
+    </footer>
+  );
+}
