5126680cb9a247b01044c74eeb0f4131202ea469
Ruslan Khuzhin <khuzh.n@gmail.com>
2021-06-10 02:58:04 +0500

    add foldersReducer and fetchFolders thunk function

---
+++ src/features/folders/foldersSlice.js
@@ -0,0 +1,30 @@
+const initialState = [];
+
+export function foldersReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'folders/foldersLoaded': {
+      return [...action.payload.folders];
+    }
+    case 'folders/folderAdded': {
+      return [...state, action.payload.folder];
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+// Thunk function
+export async function fetchFolders(dispatch, getState) {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/folders`);
+    if (!response.ok) throw new Error();
+    const folders = await response.json();
+    dispatch({
+      type: 'folders/foldersLoaded',
+      payload: { folders: folders },
+    });
+  } catch {
+    console.error('Could not fetch folders from the server');
+  }
+}
