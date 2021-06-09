49159388d976eb166dfb4f09afda406d8d8b4ec7
Ruslan Khuzhin <khuzh.n@gmail.com>
2021-06-10 02:57:26 +0500

    add notesReducer and fetchNotes thunk function

---
+++ src/features/notes/notesSlice.js
@@ -0,0 +1,30 @@
+const initialState = [];
+
+export function notesReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'notes/notesLoaded': {
+      return [...action.payload.notes];
+    }
+    case 'notes/noteAdded': {
+      return [...state, action.payload.note];
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+// Thunk function
+export async function fetchNotes(dispatch, getState) {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/notes`);
+    if (!response.ok) throw new Error();
+    const notes = await response.json();
+    dispatch({
+      type: 'notes/notesLoaded',
+      payload: { notes: notes },
+    });
+  } catch {
+    console.error('Could not fetch notes from the server');
+  }
+}
