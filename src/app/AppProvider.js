30f3fa467bf80a928e4fdbce5bcae6f9efb7d8d9
Ruslan Khuzhin <khuzh.n@gmail.com>
2021-06-10 03:03:14 +0500

    replace react context API with redux store

--- src/app/AppProvider.js
+++ src/app/AppProvider.js
@@ -1,51 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import AppContext from 'app/AppContext';
+import { store } from 'app/store';
+import { Provider } from 'react-redux';
+import React, { useEffect } from 'react';
+import { fetchNotes } from 'features/notes/notesSlice';
+import { fetchFolders } from 'features/folders/foldersSlice';
 
 export default function AppProvider({ children }) {
-  const [folders, setFolders] = useState([]);
-  const [notes, setNotes] = useState([]);
-
   useEffect(() => {
-    fetchFolders();
-    fetchNotes();
+    store.dispatch(fetchFolders);
+    store.dispatch(fetchNotes);
   }, []);
 
-  function fetchFolders() {
-    fetch(`${process.env.REACT_APP_API_URL}/folders`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error();
-        }
-        return response.json();
-      })
-      .then((folders) => {
-        setFolders(folders);
-      })
-      .catch(() => alert('Could not fetch folders from the server'));
-  }
-
-  function fetchNotes() {
-    fetch(`${process.env.REACT_APP_API_URL}/notes`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error();
-        }
-        return response.json();
-      })
-      .then((notes) => {
-        setNotes(notes);
-      })
-      .catch(() => alert('Could not fetch notes from the server'));
-  }
-
-  return (
-    <AppContext.Provider
-      value={{
-        folders,
-        notes,
-      }}
-    >
-      {children}
-    </AppContext.Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 }
